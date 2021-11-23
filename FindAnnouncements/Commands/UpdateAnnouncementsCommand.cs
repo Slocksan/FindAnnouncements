c507419ed5cf776fec5e7fdf0b7e748c34b21b7c
Slocksan <977005@gmail.com>
2021-11-23 17:19:18 +0500

    Обновление объявлений

---
+++ FindAnnouncements/Commands/UpdateAnnouncementsCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FindAnnouncements.Models;
+using FindAnnouncements.ViewModel;
+
+namespace FindAnnouncements.Commands
+{
+    class UpdateAnnouncementsCommand : CommandBase
+    {
+        private readonly AnnouncemetsDeskViewModel _announcemetsDeskViewModel;
+        public override void Execute(object parameter)
+        {
+            _announcemetsDeskViewModel.Announcements = AnnouncementService.GetAllAnnouncements();
+        }
+
+        public UpdateAnnouncementsCommand(AnnouncemetsDeskViewModel announcemetsDeskViewModel)
+        {
+            _announcemetsDeskViewModel = announcemetsDeskViewModel;
+            
+        }
+    }
+}
