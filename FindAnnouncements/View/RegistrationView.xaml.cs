213b66149ec708e6731b8e23d406b03c4f1f8c83
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-12-15 21:10:59 +0500

    Design

--- FindAnnouncements/View/RegistrationView.xaml.cs
+++ FindAnnouncements/View/RegistrationView.xaml.cs
@@ -24,5 +24,10 @@ namespace FindAnnouncements.View
         {
             InitializeComponent();
         }
+
+        private void Имя_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
